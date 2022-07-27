a837b8a57b788af6160d68af479f230d7cbf3e09
nhi-nguyen-csis <nhi.nguyen.csis@gmail.com>
2022-07-26 22:16:17 -0700

    added Caesar Hacker Project
    Signed-off-by: nhi-nguyen-csis <nhi.nguyen.csis@gmail.com>

---
+++ Projects/Caesar Hacker/Caesar_Hacker.py
@@ -0,0 +1,24 @@
+
+def generateEncrypt(key, message):
+    hashMessage = [''] * len(message)
+    for i in range(len(message)):
+        # if current letter is alphabetic, then dencrypt it
+        if message[i].isalpha():
+            iLetter = ord(message[i]) - key
+            if iLetter < ord('A'):
+                # the below code handle the wrap-around
+                iLetter = ord('Z') - (( ord('A') % iLetter) - 1)
+            hashMessage[i] = chr(iLetter)
+        else: # If not, then do nothing
+            hashMessage[i] = message[i]
+    return "".join(hashMessage)
+
+def brute_force(message):
+    for i in range(27):
+        print(f"Key #{i}: {generateEncrypt(i, message)}")
+
+def main():
+    message = input("Enter the encrypted Caesar cipher message to hack: ").upper()
+    brute_force(message)
+if __name__ == '__main__':
+    main()
---
+++ Projects/Caesar Hacker/README.md
@@ -0,0 +1,9 @@
+### This project is from "Automate the Boring Stuff with Python"
+
+Here are some notes:
+
+- We assume that Caesar cipher only works on uppercase letters, and 
+it encrypts/decrypts alphabetical letters only. Thus, we use 
+a brute force approach to search for 26 posibilities and print them all
+
+- I use modulus to handle the wrap-around for decrypting the message in the program
